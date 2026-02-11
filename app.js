f7080ef45b2379c8b6677d41ee703387cf02322c
CedricJankiewicz <pm14cau@eduvaud.ch>
2026-02-11 15:04:43 +0100

    fix:changed the app.js to work correctly also removed french

--- app.js
+++ app.js
@@ -8,68 +8,30 @@
  * Version :                0.1.3
  **********************************************************************************************************************/
 "use strict";
+
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from "./swagger.js";
 const app = express();
 const port = 3000;
 
-//  read JSON
+// read JSON
 app.use(express.json());
 
 
-// routes swagger
+// swagger route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// --- Routes de l'API ---
-
-/**
- * @openapi
- * /:
- * get:
- * summary: Message de bienvenue
- * responses:
- * 200:
- * description: Succès
- * content:
- * text/plain:
- * schema:
- * type: string
- * example: "Bienvenue sur l'API !"
- */
+// API routes
 app.get('/', (req, res) => {
-    res.send('Bienvenue sur l\'API !');
+    res.send('Welcome on the videogame plateform API');
 });
 
-/**
- * @openapi
- * /users:
- * get:
- * summary: Récupère la liste des utilisateurs
- * responses:
- * 200:
- * description: Liste des utilisateurs récupérée
- * content:
- * application/json:
- * schema:
- * type: array
- * items:
- * type: object
- * properties:
- * id:
- * type: integer
- * nom:
- * type: string
- */
-app.get('/users', (req, res) => {
-    res.json([
-        { id: 1, nom: 'Alice' },
-        { id: 2, nom: 'Bob' }
-    ]);
-});
+// router
+//app.use('/api/Users', usersRouter);
 
-// start server
+// start the server
 app.listen(port, () => {
-    console.log(`✅ Serveur démarré sur http://localhost:${port}`);
-    console.log(`📖 Documentation disponible sur http://localhost:${port}/api-docs`);
+    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Documentation at http://localhost:${port}/api-docs`);
 });
