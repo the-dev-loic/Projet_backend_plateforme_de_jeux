811a8b69fc5c39f62f18ba1be632ba17471caad1
CedricJankiewicz <pm14cau@eduvaud.ch>
2026-02-24 16:29:02 +0100

    feat: swagger documentation

--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,6 @@
 /***********************************************************************************************************************
- * Program name :           app.js
- * Description :            Main app of the API
+ * Program name :           genres.js
+ * Description :            route for genres table
  * Author :                 Cédric Jankiewicz
  * Creation date :          24.02.2026
  * Modified by :            -
@@ -10,6 +10,122 @@
 import express from 'express';
 const router = express.Router();
 import { CRUD } from "../database/database-connection.js";
+
+/**
+ * @swagger
+ * /api/genres:
+ *   get:
+ *     summary: Get all genres
+ *     description: Retrieve a list of genres.
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved genres
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Page not found
+ *       500:
+ *         description: Internal server error
+ *
+ *   post:
+ *     summary: Create a genre
+ *     description: Create a new genre.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: rpg
+ *     responses:
+ *       201:
+ *         description: genre created successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ *
+ * /api/genres/{id}:
+ *   get:
+ *     summary: Get a single genre
+ *     description: Retrieve a specific genre by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the genre
+ *         example: 1
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved the genre
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: genre not found
+ *       500:
+ *         description: Internal server error
+ *
+ *   put:
+ *     summary: Edit a genre
+ *     description: Edit a genre by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the genre
+ *         example: 1
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: rpg
+ *     responses:
+ *       200:
+ *         description: Successfully edited the genre
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: genre not found
+ *       500:
+ *         description: Internal server error
+ *
+ *   delete:
+ *     summary: Delete a genre
+ *     description: Delete a specific genre by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the genre
+ *         example: 1
+ *     responses:
+ *       200:
+ *         description: genre deleted successfully
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: genre not found
+ *       500:
+ *         description: Internal server error
+ */
 
 router.get('/', async (req, res) => {
     let genres = await CRUD.getAllFromEntity("genres")
