93fa8b5f1809831d49adfddaf1910e460287aea9
CedricJankiewicz <pm14cau@eduvaud.ch>
2026-03-17 15:52:08 +0100

    feat: installed bcrypt

---
+++ functions/hash.js
@@ -0,0 +1,18 @@
+/***********************************************************************************************************************
+ * Program name :           hash.js
+ * Description :            function to hash passwords
+ * Author :                 Cédric Jankiewicz
+ * Creation date :          17.03.2026
+ * Modified by :            -
+ * Modification date :      -
+ * Version :                0.1.0
+ **********************************************************************************************************************/
+import bcrypt from 'bcrypt';
+
+export async function hashPassword(){
+
+}
+
+export async function verifyPassword(){
+
+}
