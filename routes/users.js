302236e2c212ef5050802654c42c87c703322a64
CedricJankiewicz <pm14cau@eduvaud.ch>
2026-03-17 15:35:35 +0100

    feat: unit test for users

--- routes/users.js
+++ routes/users.js
@@ -63,14 +63,13 @@ const router = express.Router();
 *         description: Internal server error
 */
 router.post('/', async (req, res) => {
-    res.json(req.body);
     const data = Object.values(req.body);
     if (data[0] === "" || !data[0] || data[0].length > 45) {
         res.status(400).json({error: "invalid name, name need to be under 45 characters"});
         return;
     }
     let response = await CRUD.createInEntity("users", ['username', 'email', 'password'], data);
-    res.status(200).json(response)
+    res.status(201).json(response)
 })
 
 /**
@@ -292,7 +291,7 @@ router.delete('/:id', async (req, res) => {
         return;
     }
     let response = await CRUD.deleteFromEntity("users", id)
-    res.status(200).json(response)
+    res.status(204).json(response)
 })
 
 export default router;
